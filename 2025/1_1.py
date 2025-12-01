98a11f2c5336b2a35ad8e6facc556ef3d1e80fd3
0xlua <dev@lukasjordan.com>
2025-12-01 19:51:40 +0100

    make script executable & change input file name

--- 2025/1_1.py
+++ 2025/1_1.py
@@ -1,10 +1,12 @@
-with open("input.txt") as input:
+#!/usr/bin/env -S uv run
+
+with open("input") as input:
     dial = 50
     result = 0
     for rot in input:
         dir = 1 if rot.startswith("R") else -1
         steps = int(rot[1:])
         dial = (dial + dir * steps) % 100
-        if (dial == 0):
+        if dial == 0:
             result += 1
     print(result)
