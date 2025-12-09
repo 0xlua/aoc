d518326099f56a809d9b1b01d8bf790d056312ee
Lukas <31389918+0xlua@users.noreply.github.com>
2025-12-09 09:42:00 +0100

    2025 day 9 part 1

---
+++ 2025/9_1.py
@@ -0,0 +1,11 @@
+from itertools import combinations
+
+def area(rect):
+    a, b = rect
+    return (abs(a[1] - b[1]) + 1) * (abs(b[0] - a[0]) + 1)
+
+
+with open("input.txt") as input:
+    tiles = [[*map(int, tile.split(","))] for tile in input.read().splitlines()]
+    result = max(map(area, combinations(tiles, 2)))
+    print(result)
